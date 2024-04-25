22f0382181db1a17627580ac8c0cbbd2b81ac537
NayoungKim1212 <kkd04250@naver.com>
2024-04-26 01:35:10 +0900

    feat 가입 일자 추가

---
+++ demo/src/main/java/com/example/demo/util/Timestamped.java
@@ -0,0 +1,25 @@
+package com.example.demo.util;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+	
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+}
